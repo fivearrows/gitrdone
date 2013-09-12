fe4f1561b857873c5c6ecb861f31557399040535
Paul Miller <paul@fivearrowsnh.com>
2013-09-12 02:03:12 +0000

    adding hierarchy to tasks

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,12 +18,14 @@ Unit.prototype = {
 //
 // Estimate - task and the associated estimate
 //
-function Estimate(dbid,name,qty,units,parent) {
+function Estimate(dbid,name,qty,units,parent,sequence,kids) {
    this.dbid=dbid;
    this.name=name;
    this.parent=parent;
    this.qty=qty;
    this.units=units;
+   this.kids=kids;
+   this.sequence=sequence;
 }
 
 Estimate.prototype= {
@@ -32,15 +34,47 @@ Estimate.prototype= {
          ' (' + this.units.to_hours(this.qty) + ' hours)';
    },
 
-   addrow: function(tbl) {
-      tb=$(tbl);
-      tr=document.createElement('tr');
-      td=document.createElement('td');
-      td.innerText=this.dump();
+   addrow: function(tbl,spacer,prefix,level) {
+      var p = prefix ? (prefix+'.') : ''
+      var l = level || 0;
+      var tb=$(tbl);
+      var tr=document.createElement('tr');
+      var td=document.createElement('td');
+      var s=document.createElement('span');
+      var im=document.createElement('img');
+      im.src=spacer.src;
+      im.height=spacer.height;
+      im.width=(l * 35);
+      td.appendChild(im);
+      s.innerText='('+l+'): '+ p + this.dump();
+      td.appendChild(s);
       tr.appendChild(td);
       tb.appendChild(tr);
       tr.id='EstimateRow' + this.dbid;
+      p=p+this.dbid;
+      this.kids.each(function(kid) { 
+         k=estimate_by_id(rows,kid);
+	 if(k) {
+            k.addrow(tbl,spacer,p,l+1);
+	 }
+      });
+   },
+
+   toplevel: function() {
+      return(this.parent == 0);
    }
+}
+
+function estimate_by_id(rows, a) {
+   return rows.find(function(b) { return b.dbid == a; });
+}
+
+function sort_by_sequence(a,b) {
+   return a.sequence - b.sequence ;
+}
+
+function filter_toplevel(a) {
+   return a.toplevel();
 }
 
 function toggle_subs(dbid) {
